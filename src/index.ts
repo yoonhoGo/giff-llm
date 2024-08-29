4e39591149ee5e0307af94c5e38529d6727cb89a
고윤호 <lylisha@gmail.com>
2024-08-29 21:27:07 +0900

    feat: prompt 추가

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,59 @@
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { Ollama } from "@langchain/ollama";
+import { checkbox, input } from "@inquirer/prompts";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { ChatOllama } from "@langchain/ollama";
 import simpleGit from "simple-git";
 
-const llm = new Ollama({
+const llm = new ChatOllama({
   model: "llama3.1",
   temperature: 0,
   maxRetries: 2,
 });
+const parser = new StringOutputParser();
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "Answer as markdown in Korean."],
+  [
+    "system",
+    "Please rate the risk score of each changed content from 0 to 1 when you answer the question.",
+  ],
+  [
+    "system",
+    "Please show the risk score up to the second decimal place when you answer the question.",
+  ],
+  [
+    "system",
+    "Please indicate the risk score with parentheses when you answer the question.",
+  ],
+  [
+    "system",
+    "Please show the risk score of each changed content when you answer the question.",
+  ],
+  ["system", "This is git diff response: {response}"],
+  ["human", "{input}"],
+])
+  .pipe(llm)
+  .pipe(parser);
 
 async function main() {
-  const response = await simpleGit(process.cwd()).diff([
-    "--name-only",
-    "package.json",
-  ]);
+  const fileList = await simpleGit(process.cwd())
+    .diff(["--name-only"])
+    .then((diff) => diff.split("\n").filter(Boolean));
 
-  console.log(response);
+  const checkboxAnswer = await checkbox({
+    message: "Select modified files",
+    choices: fileList.map((file) => ({ name: file, value: file })),
+  });
 
-  // const assi = await llm.invoke([
-  //   new SystemMessage("git diff 메시지입니다."),
-  //   new SystemMessage(response),
-  //   new SystemMessage("한국어로 대답해주세요."),
-  //   new HumanMessage("달라진 차이점이 무엇입니까?"),
-  // ]);
+  const inputAnswer = await input({ message: "Write prompts 》" });
 
-  // console.log(assi);
+  const diff = await simpleGit(process.cwd()).diff(checkboxAnswer);
+
+  const assi = await prompt.invoke({
+    response: diff,
+    input: inputAnswer,
+  });
+
+  console.log(assi);
 }
 
 main();
